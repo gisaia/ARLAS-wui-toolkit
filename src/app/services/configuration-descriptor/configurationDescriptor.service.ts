077efbb271e6ad6320ba4b993e26d2d91281c74e
MohamedHamou <mohamed.hamou@gisaia.com>
2019-04-16 16:23:15 +0200

    get collections

--- src/app/services/configuration-descriptor/configurationDescriptor.service.ts
+++ src/app/services/configuration-descriptor/configurationDescriptor.service.ts
@@ -1,25 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ArlasConfigService, ArlasCollaborativesearchService } from '../startup/startup.service';
-import { Observable } from 'rxjs';
+import { ArlasConfigService, ArlasCollaborativesearchService, ArlasExploreApi, ArlasCollectionApi } from '../startup/startup.service';
+import { Observable, from } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
+import { Configuration, CollectionReference } from 'arlas-api';
+import * as portableFetch from 'portable-fetch';
+
 
 @Injectable()
 export class ArlasConfigurationDescriptor {
 
+  constructor(private collaborativesearchService: ArlasCollaborativesearchService, private configService: ArlasConfigService) {}
 
-  constructor(private collaborativesearchService: ArlasCollaborativesearchService, private configService: ArlasConfigService) {
+  public getAllCollections(): Observable<Array<string>> {
+    const configuration: Configuration = new Configuration();
+    const arlasCollectionsApi = new ArlasCollectionApi(
+      configuration,
+      this.configService.getValue('arlas.server.url'),
+      portableFetch
+    );
+    return <Observable<Array<string>>>from(arlasCollectionsApi.getAll1()).pipe(
+      map((collections: Array<CollectionReference>) => collections.map((collection: CollectionReference) => collection.collection_name).filter(collection => collection !== 'metacollection'))
+    );
   }
-
 
   /**
    *
    * @param types
    * @description Returns all the fields which types are in `types` param. If no `types` is specified, then all the fields are returned
    */
-  public getFields(types?: Array<string>):  Observable<any> {
-    console.log('appel')
+  public getFields(types?: Array<string>): Observable<any> {
     return this.collaborativesearchService.describe(this.configService.getValue('arlas.server.collection.name')).pipe(
-      map(description => { console.log('test'); return this.getFieldProperties(description.properties)}),
+      map(description => this.getFieldProperties(description.properties)),
       map(fields => types ? fields.filter(field => types.find(type => type == field.type) !== undefined).map(field => field.label)
         : fields.map(field => field.label))
     );
