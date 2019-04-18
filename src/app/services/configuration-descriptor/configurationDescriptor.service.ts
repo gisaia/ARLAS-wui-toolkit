d423738b468cecc1584f57cd9df7ccbfb6dcd01e
MohamedHamou <mohamed.hamou@gisaia.com>
2019-04-18 10:51:02 +0200

    Add documentation to methods

--- src/app/services/configuration-descriptor/configurationDescriptor.service.ts
+++ src/app/services/configuration-descriptor/configurationDescriptor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ArlasConfigService, ArlasCollaborativesearchService, ArlasExploreApi, ArlasCollectionApi } from '../startup/startup.service';
 import { Observable, from } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Configuration, CollectionReference } from 'arlas-api';
 import * as portableFetch from 'portable-fetch';
 
@@ -12,8 +12,12 @@ export class ArlasConfigurationDescriptor {
   constructor(
     private collaborativesearchService: ArlasCollaborativesearchService,
     private configService: ArlasConfigService
-  ) {}
+  ) { }
 
+  /**
+   * @description Returns an Observable that contains the list of all collections
+   * declared in ARLAS-server (that is configurated in `arlas.server.url`)
+   */
   public getAllCollections(): Observable<Array<string>> {
     const configuration: Configuration = new Configuration();
     const arlasCollectionsApi = new ArlasCollectionApi(
@@ -22,42 +26,50 @@ export class ArlasConfigurationDescriptor {
       portableFetch
     );
     return <Observable<Array<string>>>from(arlasCollectionsApi.getAll1()).pipe(
-      map((collections: Array<CollectionReference>) => collections.map((collection: CollectionReference) => collection.collection_name).filter(collection => collection !== 'metacollection'))
+      map(
+        (collections: Array<CollectionReference>) => collections.map(
+          (collection: CollectionReference) => collection.collection_name
+        )
+          .filter(collection => collection !== 'metacollection')
+      )
     );
   }
 
   /**
    *
    * @param types
-   * @description Returns all the fields which types are in `types` param. If no `types` is specified, then all the fields are returned
+   * @description Returns an Observable that contains a list of the fields which types are in `types` param.
+   * If no `types` are specified, then all the fields are returned.
    */
-  public getFields(types?: Array<string>): Observable<any> {
+  public getFields(types?: Array<string>): Observable<Array<{ label: string, type: string }>> {
     const configuraiton: Configuration = new Configuration();
     const arlasExploreApi: ArlasExploreApi = new ArlasExploreApi(
-        configuraiton,
-        this.configService.getValue('arlas.server.url'),
-        portableFetch
+      configuraiton,
+      this.configService.getValue('arlas.server.url'),
+      portableFetch
     );
     this.collaborativesearchService.setExploreApi(arlasExploreApi);
     return this.collaborativesearchService.describe(this.configService.getValue('arlas.server.collection.name')).pipe(
       map(description => this.getFieldProperties(description.properties)),
-      map(fields => types ? fields.filter(field => types.find(type => type == field.type) !== undefined)
+      map(fields => types ? fields.filter(field => types.find(type => type === field.type) !== undefined)
         : fields)
     );
   }
 
-  private getFieldProperties(fieldList: any, parentPrefix?: string, arlasFields?: Array<{label: string, type: string}>, isFirstLevel?: boolean ) {
+  private getFieldProperties(fieldList: any, parentPrefix?: string,
+    arlasFields?: Array<{ label: string, type: string }>, isFirstLevel?: boolean
+  ) {
     if (!arlasFields) {
       arlasFields = new Array();
     }
-    if (isFirstLevel === undefined)  {
+    if (isFirstLevel === undefined) {
       isFirstLevel = true;
     }
     Object.keys(fieldList).forEach(fieldName => {
       if (fieldList[fieldName].type === 'OBJECT') {
         const subFields = fieldList[fieldName].properties;
         if (subFields) {
-          this.getFieldProperties(subFields,  (parentPrefix ? parentPrefix : '') + fieldName + '.', arlasFields, false);
+          this.getFieldProperties(subFields, (parentPrefix ? parentPrefix : '') + fieldName + '.', arlasFields, false);
         }
       } else {
         arlasFields.push({ label: (parentPrefix ? parentPrefix : '') + fieldName, type: fieldList[fieldName].type });
