3da90c8cf5d34cd6dd5a0a9de0c046a140639dfd
MohamedHamou <mohamed.hamou@gisaia.com>
2020-08-12 17:40:08 +0200

    Fix CI

--- src/app/components/config-manager/action-modal/action-modal.component.spec.ts
+++ src/app/components/config-manager/action-modal/action-modal.component.spec.ts
@@ -1,14 +1,59 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionModalComponent } from './action-modal.component';
+import { TranslateModule, TranslateLoader, TranslateFakeLoader } from '@ngx-translate/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { GET_OPTIONS } from '../../../services/persistence/persistence.service';
+import { AuthentificationService } from '../../../services/authentification/authentification.service';
+import { getOptionsFactory } from '../../../app.module';
+import { OAuthLogger, OAuthService, UrlHelperService } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ActionModalComponent', () => {
   let component: ActionModalComponent;
   let fixture: ComponentFixture<ActionModalComponent>;
 
   beforeEach(async(() => {
+    const mockDialogRef = {
+      close: jasmine.createSpy('close')
+    };
     TestBed.configureTestingModule({
-      declarations: [ ActionModalComponent ]
+      declarations: [ ActionModalComponent ],
+      imports: [
+        TranslateModule.forRoot({loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }}),
+        MatMenuModule,
+        MatIconModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        FormsModule,
+        HttpClientModule
+      ],
+      providers: [
+        AuthentificationService,
+        OAuthService,
+        OAuthLogger,
+        UrlHelperService,
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        },
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: GET_OPTIONS,
+          useFactory: getOptionsFactory,
+          deps: [AuthentificationService]
+        }      ]
     })
     .compileComponents();
   }));
--- src/app/components/config-manager/config-menu/config-menu.module.ts
+++ src/app/components/config-manager/config-menu/config-menu.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigMenuComponent } from './config-menu.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ActionModalModule } from '../action-modal/action-modal.module';
 import { ErrorService } from '../../../services/error/error.service';
 
