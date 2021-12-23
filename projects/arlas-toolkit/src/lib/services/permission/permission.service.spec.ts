490bc1669a45846006aa9fae3a7d92ee59d8d73b
MohamedHamouGisaia <mohamed.hamou@gisaia.com>
2021-12-23 11:57:39 +0100

    Fix test + remove i18n check

--- projects/arlas-toolkit/src/lib/services/permission/permission.service.spec.ts
+++ projects/arlas-toolkit/src/lib/services/permission/permission.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { OAuthLogger, OAuthModule, OAuthService, UrlHelperService } from 'angular-oauth2-oidc';
+import { DateTimeProvider, OAuthLogger, OAuthModule, OAuthService, UrlHelperService } from 'angular-oauth2-oidc';
 import { AuthentificationService } from '../authentification/authentification.service';
 import { GET_OPTIONS } from '../persistence/persistence.service';
 import { ArlasSettingsService } from '../settings/arlas.settings.service';
@@ -13,6 +13,7 @@ describe('PermissionService', () => {
     providers: [
       ArlasSettingsService,
       OAuthService,
+      DateTimeProvider,
       OAuthLogger,
       UrlHelperService,
       AuthentificationService,
