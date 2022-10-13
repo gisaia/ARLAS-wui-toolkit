2e0022a3ee787483d9ced4b1cca68e004df54d17
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2022-10-13 18:17:27 +0200

    feat: add reset password component

--- projects/arlas-toolkit/src/lib/toolkit-routing.module.ts
+++ projects/arlas-toolkit/src/lib/toolkit-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ToolkitComponent } from './toolkit.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { ResetComponent } from './components/reset/reset.component';
+import { ForgotComponent } from './components/forgot/forgot.component';
 
 export const routes: Routes = [
-  { path: '', component: ToolkitComponent},
+  { path: '', component: ToolkitComponent },
   { path: 'callback', redirectTo: '' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'verify/:id/user/:token', component: VerifyComponent },
+  { path: 'password_forgot', component: ForgotComponent },
+  { path: 'reset/:id/user/:token', component: ResetComponent },
 ];
 
 @NgModule({
