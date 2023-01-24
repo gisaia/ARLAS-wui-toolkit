221af11aaceb4f198ae28c46d0b70e0dd0201589
MatthieuBarbet <matthieu.barbet@gisaia.com>
2023-01-24 14:27:27 +0100

    Fix : permission service return 401 on refresh token

--- projects/arlas-toolkit/src/lib/services/startup/startup.service.ts
+++ projects/arlas-toolkit/src/lib/services/startup/startup.service.ts
@@ -45,7 +45,7 @@ import { AuthentificationService, AuthentSetting, NOT_CONFIGURED } from '../auth
 import { ArlasConfigurationUpdaterService } from '../configuration-updater/configurationUpdater.service';
 import { ErrorService } from '../error/error.service';
 import { FetchInterceptorService } from '../interceptor/fetch-interceptor.service';
-import { PermissionSetting } from '../permission/permission.service';
+import { PermissionService, PermissionSetting } from '../permission/permission.service';
 import { ArlasSettingsService } from '../settings/arlas.settings.service';
 import * as arlasConfSchema from './arlasconfig.schema.json';
 import { ContributorBuilder } from './contributorBuilder';
@@ -157,6 +157,7 @@ export class ArlasStartupService {
     private http: HttpClient, private translateService: TranslateService,
     @Inject(CONFIG_UPDATER) private configUpdater,
     private persistenceService: PersistenceService,
+    private persmissionService: PermissionService,
     private errorService: ErrorService, private fetchInterceptorService: FetchInterceptorService) {
     this.configurationUpdaterService = new ArlasConfigurationUpdaterService;
   }
@@ -431,12 +432,18 @@ export class ArlasStartupService {
                 Authorization: 'bearer ' + authService.accessToken
               }
             });
+            this.persmissionService.setOptions({
+              headers: {
+                Authorization: 'bearer ' + authService.accessToken
+              }
+            });
             // ARLAS-server
             this.fetchOptions.headers = {
               Authorization: 'bearer ' + authService.accessToken
             };
           } else {
             this.persistenceService.setOptions(this.getOptions());
+            this.persmissionService.setOptions(this.getOptions());
           }
           this.collaborativesearchService.setFetchOptions(this.fetchOptions);
           resolve(settings);
@@ -444,6 +451,7 @@ export class ArlasStartupService {
 
       } else {
         this.persistenceService.setOptions(this.getOptions());
+        this.persmissionService.setOptions(this.getOptions());
         resolve(settings);
       }
     });
