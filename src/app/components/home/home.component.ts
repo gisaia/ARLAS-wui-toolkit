a8c061113f1ce9d80ce3fc4b08c2c5d8fcc11de1
MAudelGisaia <mickael.audel@gisaia.com>
2024-06-13 11:27:37 +0200

    feat: implement feedback

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -44,7 +44,7 @@ export class HomeComponent implements OnInit {
   public windowWidth = window.innerWidth;
   public spinnerOptions: SpinnerOptions = DEFAULT_SPINNER_OPTIONS;
 
-  @ViewChild('tooltip') t
+  @ViewChild('tooltip') tooltip
 
   public constructor(
     private arlasStartupService: ArlasStartupService,
@@ -137,7 +137,7 @@ export class HomeComponent implements OnInit {
       'xUnit': 'voiture',
       'yUnit': 'velo'
     };
-    this.arlasOverlayService.openHistogramTooltip({ data: time }, this.t, 0, 0, false);
+    this.arlasOverlayService.openHistogramTooltip({ data: noTime }, this.tooltip, 0, 0, false);
 
     fromEvent(window, 'resize')
       .subscribe((event: Event) => {
