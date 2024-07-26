8130a24302dafdaa06c04a52bcad8fac59e4ecd1
MAudelGisaia <mickael.audel@gisaia.com>
2024-07-26 12:47:42 +0200

    feat: init value for display name

--- projects/arlas-toolkit/src/lib/tools/collection-service-implementation.service.ts
+++ projects/arlas-toolkit/src/lib/tools/collection-service-implementation.service.ts
@@ -1,20 +1,21 @@
 import { CollectionUnit } from './utils';
-import { map, Observable } from 'rxjs';
+import { catchError, EMPTY, first, map, Observable } from 'rxjs';
 import { BaseCollectionService } from 'arlas-web-components';
 import { Injectable } from '@angular/core';
 import { ArlasCollaborativesearchService, ArlasConfigService } from '../services/startup/startup.service';
 import { CollectionReferenceDescription } from 'arlas-api';
-import { CollectionDisplayNames } from 'arlas-api/api';
 
 @Injectable()
 export class CollectionServiceImplementation extends BaseCollectionService {
   private appUnits: Map<string, CollectionUnit> = new Map();
+  private displayName: Map<string, string> = new Map();
   public constructor(private collaborativeService: ArlasCollaborativesearchService, private configService: ArlasConfigService) {
     super();
-    this._setUnits();
+    this._initUnits();
+    this._initDisplayNames();
   }
 
-  protected _setUnits() {
+  protected _initUnits() {
     const appUnits = this.configService.getValue('arlas-wui.web.app.units') ?
       this.configService.getValue('arlas-wui.web.app.units') : [];
     appUnits.forEach((collectionUnit: CollectionUnit) => this.appUnits.set(collectionUnit.collection, collectionUnit));
@@ -31,6 +32,22 @@ export class CollectionServiceImplementation extends BaseCollectionService {
     /** end of retrocompatibility code */
   }
 
+  protected _initDisplayNames(){
+    this.appUnits.forEach(collectionUnit => {
+      this.getDisplayNameFromDescribe(collectionUnit.collection)
+        .pipe(
+          first(),
+          map(displayName => {
+            this.displayName.set(collectionUnit.collection, displayName);
+          }),
+          catchError(err => {
+            return EMPTY;
+          })
+        ).subscribe();
+    })
+  }
+
+
   public getUnit(collectionName: string): string | null {
     if (this.appUnits.has(collectionName)) {
       return this.appUnits.get(collectionName).unit;
@@ -38,16 +55,17 @@ export class CollectionServiceImplementation extends BaseCollectionService {
     return null;
   };
 
-  public getDisplayName(collectionName: string): Observable<CollectionDisplayNames | undefined> {
-    /* could be improved with a cache if we have a lot of display name in the app
-    if(!this.describeCache.has(collectionName)){
-      const cache= this.collaborativeService.describe(collectionName)
-        .pipe(
-          shareReplay({bufferSize:1, refCount: true})
-        );
-      this.describeCache.set(collectionName, cache);
-    } */
-    return  this.collaborativeService.describe(collectionName)
-      .pipe(map((result: CollectionReferenceDescription) => result.params?.display_names));
+  public getDisplayName(collectionName: string): string | undefined {
+    return this.displayName.get(collectionName) || collectionName;
+  }
+
+  public getDisplayNameFromDescribe(collectionName: string): Observable<string> {
+      return this.collaborativeService.describe(collectionName)
+        .pipe(map((result: CollectionReferenceDescription) => {
+          if (result.params?.display_names) {
+            return result.params?.display_names?.collection
+          }
+          return collectionName;
+        }));
   }
 }
