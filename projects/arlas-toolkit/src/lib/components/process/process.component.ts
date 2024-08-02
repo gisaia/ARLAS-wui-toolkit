b53f77ceb13676b7d466338b5321534ca697e682
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2024-08-02 14:58:31 +0200

    ci: add license check

--- projects/arlas-toolkit/src/lib/components/process/process.component.ts
+++ projects/arlas-toolkit/src/lib/components/process/process.component.ts
@@ -1,3 +1,22 @@
+/*
+ * Licensed to Gisaïa under one or more contributor
+ * license agreements. See the NOTICE.txt file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Gisaïa licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
@@ -5,7 +24,6 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { ProcessService } from '../../services/process/process.service';
 import { ProcessInput, Process, ProcessInputs, ProcessOutput, ProcessStatus } from '../../tools/process.interface';
 import { Observable, Subject, Subscription, takeUntil, timer } from 'rxjs';
-import { filter } from 'rxjs/operators';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
 
--- projects/arlas-toolkit/src/lib/tools/jwt.interceptor.ts
+++ projects/arlas-toolkit/src/lib/tools/jwt.interceptor.ts
@@ -1,9 +1,28 @@
+/*
+ * Licensed to Gisaïa under one or more contributor
+ * license agreements. See the NOTICE.txt file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Gisaïa licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { PROTECTED_IMAGE_HEADER } from 'arlas-web-components';
+import { Observable } from 'rxjs';
+import { ArlasIamService } from '../services/arlas-iam/arlas-iam.service';
 import { AuthentificationService } from '../services/authentification/authentification.service';
-import { ARLAS_ORG_FILTER, ArlasIamService } from '../services/arlas-iam/arlas-iam.service';
 import { ArlasSettingsService } from '../services/settings/arlas.settings.service';
 
 @Injectable()
