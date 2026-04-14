b7a69599133ee1b0e52bafee6eb4f08f03b4a020
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-04-14 15:26:40 +0200

    fix: improve typing

--- projects/arlas-toolkit/src/lib/components/config-manager/config-menu/config-menu.component.ts
+++ projects/arlas-toolkit/src/lib/components/config-manager/config-menu/config-menu.component.ts
@@ -51,7 +51,7 @@ export class ConfigMenuComponent {
 
   @Input() public zone: string;
 
-  @Output() public actionExecutedEmitter = new Subject();
+  @Output() public actionExecutedEmitter = new Subject<ConfigAction>();
 
   public ConfigAction = ConfigActionEnum;
 
