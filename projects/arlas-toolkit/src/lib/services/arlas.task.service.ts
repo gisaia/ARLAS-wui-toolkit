d9f463bb042efd656ad4318a1cc7130778480341
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-08-07 16:06:32 +0200

    feat: augment TaskService configuration

--- projects/arlas-toolkit/src/lib/services/arlas.task.service.ts
+++ projects/arlas-toolkit/src/lib/services/arlas.task.service.ts
@@ -1,8 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Task, TaskService } from 'arlas-web-contributors';
+import { AvailableProcess, Task, TaskService } from 'arlas-web-contributors';
 import { Observable, of } from 'rxjs';
 import { GET_OPTIONS, GetOptions } from '../tools/utils';
+
+export interface TaskSettings {
+  enabled: boolean;
+  url: string;
+  collections: string[];
+  ignoredProcess?: string[];
+  taskRetrievalTimer?: number;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +21,8 @@ export class ArlasTaskService implements TaskService {
   private readonly getOptions = inject(GET_OPTIONS);
 
   private readonly taskUrls = new Map<string, string>();
+  public ignoredProcess = new Set<AvailableProcess>();
+  public taskRetrievalTimer = 5000;
 
   public constructor() {
     this.setOptions(this.getOptions());
@@ -22,8 +32,24 @@ export class ArlasTaskService implements TaskService {
     this.options = options;
   }
 
-  public setTaskUrl(url: string, collection: string) {
-    this.taskUrls.set(collection, url);
+  public setSettings(settings: TaskSettings) {
+    if (!settings.enabled) {
+      return;
+    }
+
+    // Check if the user is allowed to fetch the jobs
+    this.http.get(settings.url + '/jobs', this.options)
+      .subscribe({
+        next: () => {
+          settings.collections.forEach(collection => {
+            this.taskUrls.set(collection, settings.url);
+          });
+
+          this.ignoredProcess = new Set<AvailableProcess>();
+          settings.ignoredProcess?.forEach(p => this.ignoredProcess.add(p as AvailableProcess));
+          this.taskRetrievalTimer = settings.taskRetrievalTimer ?? 5000;
+        }
+      });
   }
 
   public getTasks(collection: string, identifier: string): Observable<Task[]> {
