78831d00aacb6c4bd611f95bc84f0a055ca4248a
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-08-07 17:03:45 +0200

    chore: remove unused import + fix vulnerabilities & tests

--- projects/arlas-toolkit/src/lib/services/arlas.task.service.spec.ts
+++ projects/arlas-toolkit/src/lib/services/arlas.task.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it } from 'vitest';
+import { GET_OPTIONS } from '../tools/utils';
 import { ArlasTaskService } from './arlas.task.service';
 
 describe('ArlasTaskService', () => {
   let service: ArlasTaskService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: GET_OPTIONS, useValue: () => { } }
+      ]
+    });
     service = TestBed.inject(ArlasTaskService);
   });
 
