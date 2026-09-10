18fa2288ee0f2f12919f981202ba09669a921d4e
MAudelGisaia <mickael.audel@gisaia.com>
2026-09-10 11:55:14 +0200

    fix: check that darkmode is enabled before add it.

--- projects/arlas-toolkit/src/lib/services/theme.service.ts
+++ projects/arlas-toolkit/src/lib/services/theme.service.ts
@@ -17,7 +17,8 @@
  * under the License.
  */
 
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { ArlasSettingsService } from './settings/arlas.settings.service';
 
 @Injectable({
   providedIn: 'root'
@@ -25,8 +26,15 @@ import { Injectable, signal } from '@angular/core';
 export class ThemeService {
   private readonly darkMode = signal(false);
   private readonly THEME_KEY = 'dark-theme-enabled';
+  private readonly settingsService = inject(ArlasSettingsService);
+  private readonly isDarkThemeEnabled = this.settingsService.isDarkThemeEnabled();
 
   public applyThemePreference() {
+    // No dark mode enabled.
+    if(!this.isDarkThemeEnabled){
+      return;
+    }
+
     const saved = localStorage.getItem(this.THEME_KEY);
     this.darkMode.set(saved === 'true');
     if (this.darkMode()) {
@@ -45,7 +53,7 @@ export class ThemeService {
     this.darkMode.set(!this.darkMode());
     localStorage.setItem(this.THEME_KEY, this.darkMode().toString());
 
-    if (this.darkMode()) {
+    if (this.darkMode() && !body.classList.contains('dark-theme')) {
       body.classList.add('dark-theme');
     } else {
       body.classList.remove('dark-theme');
